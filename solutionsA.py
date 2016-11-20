431ba255e59246dbd5a43c9890c57704844dc8cd
YuChia <erwin.yuchia.chen@gmail.com>
2016-11-20 15:33:20 -0800

    fix unigram of step 1, should not add start sym when cal unigram

--- solutionsA.py
+++ solutionsA.py
@@ -13,21 +13,25 @@ MINUS_INFINITY_SENTENCE_LOG_PROB = -1000
 # This function outputs three python dictionaries, where the keys are tuples expressing the ngram and the value is the log probability of that ngram
 def calc_probabilities(training_corpus):
     unigrams_full_list = []
+    unigrams_list_no_start = []
     bigrams_full_list = []
     trigrams_full_list = []
     for sentence in training_corpus:
         tokens = sentence.split(' ')[0:-1]
-        tokens.insert(0,START_SYMBOL)
         tokens.append(STOP_SYMBOL)
+        unigrams_list_no_start.extend(tokens)
+        tokens.insert(0,START_SYMBOL)
+
         unigrams_full_list.extend(tokens)
         bigrams_full_list.extend(list(nltk.bigrams(tokens)))
         trigrams_full_list.extend(list(nltk.trigrams(tokens)))
 
     uni_fdist = dict(nltk.FreqDist(unigrams_full_list))
+    uni_fdist_no_start = dict(nltk.FreqDist(unigrams_list_no_start))
     bi_fdist = dict(nltk.FreqDist(bigrams_full_list))
     tri_fdist = dict(nltk.FreqDist(trigrams_full_list))
 
-    unigram_p = {(k,): math.log( float(v)/len(unigrams_full_list), 2) for k,v in uni_fdist.items()}
+    unigram_p = {(k,): math.log( float(v)/len(unigrams_list_no_start), 2) for k,v in uni_fdist_no_start.items()}
     bigram_p = {k: math.log( float(v)/uni_fdist[k[0]], 2) for k,v in bi_fdist.items()}
     trigram_p = {k: math.log( float(v)/bi_fdist[(k[0],k[1])], 2) for k,v in tri_fdist.items()}
 
